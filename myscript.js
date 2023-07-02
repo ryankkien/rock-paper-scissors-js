e0b70d9dddb321a18ab236fa081c01d9ebf4ad9c
ryankkien <ryankien@ucla.edu>
2023-07-02 15:13:10 -0700

    Update myscript.js
    Filled out rock paper scissors game in java script.

---
+++ myscript.js
@@ -0,0 +1,49 @@
+function getComputerChoice{
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors'
+    }
+}
+
+function oneRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice == computerChoice) {
+        return "It's a tie!";
+    }
+    if (playerChoice == 'rock') {
+        if (computerChoice == 'paper') {
+            return 'You lose!';
+        } else {
+            return 'You win!';
+        }
+    }
+    if (playerChoice == 'paper') {
+        if (computerChoice == 'scissors') {
+            return 'You lose!';
+        } else {
+            return 'You win!';
+        }
+    }
+    if (playerChoice == 'scissors') {
+        if (computerChoice == 'rock') {
+            return 'You lose!';
+        } else {
+            return 'You win!';
+        }
+    }
+    else {
+        return 'Invalid input!';
+    }
+}
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        result = oneRound(prompt("Rock, paper, or scissors?"), getComputerChoice());
+        console.log(result);    
+    }
+}
